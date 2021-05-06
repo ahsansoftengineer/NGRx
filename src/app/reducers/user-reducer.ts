a857c4849d484d8ec5b67fc8bf4ce4bd8be362de
Muhammad Ahsan <mahsansoftengineer@indusvalley.io>
2021-05-06 14:18:48 +0500

    Using Entities and Id in REDUX

--- src/app/reducers/user-reducer.ts
+++ src/app/reducers/user-reducer.ts
@@ -1,3 +1,4 @@
+import { createSelector } from '@ngrx/store';
 import { Action } from '../actions';
 import {
   USER_ADD,
@@ -8,19 +9,24 @@ import {
   USER_UPDATE,
 } from '../actions/user-action';
 import { IUser } from '../models/user';
+import { StoreUtility } from '../utils/store-utility';
 
 export interface UserReducerState {
   loading: boolean;
   loaded: boolean;
   error: boolean;
-  users: IUser[];
+  entities: { [id: number]: any };
+  ids: number[];
+  // users: IUser[];
 }
 
 const initialState: UserReducerState = {
   loaded: false,
   loading: false,
   error: false,
-  users: [],
+  entities: {},
+  ids: [],
+  // users: [],
 };
 export function UserReducer(
   state = initialState,
@@ -32,35 +38,64 @@ export function UserReducer(
     }
     // Collected All User Except That needs to be Modified
     case USER_ADD: {
-      const newUsers: IUser[] = state.users.concat(action.payload.data);
-      return { ...state, ...{ users: newUsers } };
+      const user: IUser = action.payload.data;
+      const entity = { [user.id]: user };
+      const newEntities = { ...state.entities, ...entity };
+      const newIds = StoreUtility.filterDuplicate([...state.ids, user.id]);
+      return { ...state, ...{ entities: newEntities, ids: newIds } };
+      // const newUsers: IUser[] = state.users.concat(action.payload.data);
+      // return { ...state, ...{ users: newUsers } };
     }
     case USER_UPDATE: {
-      const users: IUser[] = state.users.filter(
-        (data) => data.id !== action.payload.id
-      );
-      const newUsers: IUser[] = users.concat(action.payload.data);
-      return { ...state, ...{ users: newUsers } };
+      const user: IUser = action.payload.data;
+      const entity = { [user.id]: user };
+      const updatedEntities = { ...state.entities, ...entity };
+      return { ...state, ...{ entities: updatedEntities } };
+
+      // const users: IUser[] = state.users.filter(
+      //   (data) => data.id !== action.payload.id
+      // );
+      // const newUsers: IUser[] = users.concat(action.payload.data);
+      // return { ...state, ...{ users: newUsers } };
     }
     case USER_DELETE: {
-      const users: IUser[] = state.users.filter(
-        (data) => data.id !== action.payload.id
-      );
-      return { ...state, ...{ users } };
+      const id = action.payload.id;
+      const newIds = state.ids.filter((elem) => elem !== id);
+      const newEntities = StoreUtility.removeKey(state.entities, id);
+      return { ...state, ...{ entities: newEntities, ids: newIds } };
+
+      // const users: IUser[] = state.users.filter(
+      //   (data) => data.id !== action.payload.id
+      // );
+      // return { ...state, ...{ users } };
     }
 
     case USER_LIST_ERROR: {
       return { ...state, error: true, loading: false };
     }
     case USER_LIST_SUCCESS: {
-      const updatedUsers = state.users.concat(action.payload.data);
-      return {
-        ...state,
-        loading: false,
+      const users: IUser[] = action.payload.data;
+      const obj = StoreUtility.normalize(users);
+      const newEntities = { ...state.entities, ...obj };
+      const ids = users.map((user) => user.id);
+      const newIds = StoreUtility.filterDuplicate([...state.ids, ...ids]);
+      const userReducerState: UserReducerState = {
         loaded: true,
+        loading: false,
         error: false,
-        users: updatedUsers,
+        entities: newEntities,
+        ids: newIds,
       };
+      return { ...state, ...userReducerState };
+
+      // const updatedUsers = state.users.concat(action.payload.data);
+      // return {
+      //   ...state,
+      //   loading: false,
+      //   loaded: true,
+      //   error: false,
+      //   users: updatedUsers,
+      // };
     }
     default: {
       return state;
@@ -71,5 +106,9 @@ export function UserReducer(
 // Selectors for Reducers
 export const getLoading = (state: UserReducerState) => state.loading;
 export const getLoaded = (state: UserReducerState) => state.loaded;
-export const getUsers = (state: UserReducerState) => state.users;
-export const getUserError = (state: UserReducerState) => state.error;
+const getEntities = (state: UserReducerState) => state.entities;
+const getIds = (state: UserReducerState) => state.ids;
+export const getUsers = createSelector(getEntities, getIds, 
+  (entities, ids) => ids.map(id => entities[id]));
+  export const getUserError = (state: UserReducerState) => state.error;
+// export const getUsers = (state: UserReducerState) => state.users;
---
+++ src/app/utils/store-utility.ts
@@ -0,0 +1,36 @@
+export class StoreUtility {
+  // [{id,...}, {id,...}] -> nomal array
+  static normalize(entityArray: Entity[]) {
+    return entityArray.reduce((previousValue, currentValue) => {
+      return {...previousValue, ...{[currentValue.id]: currentValue}}
+    })
+  }
+    // entities: {id: {}} -> normalized format
+  // [{1: {}},{2: {}}, ...]
+  static unNormalized(entities: {id: any}){
+    if(!entities){
+      return []
+    }else {
+      return Object.keys(entities).map(key => entities[key])
+    }
+  }
+  // [1,2,3,4,5,1]
+  static filterDuplicate(ids:number[]){
+    return ids.filter ((elem, index, self) => index === self.indexOf(elem))
+  }
+  // Delete Entity
+  static removeKey(entities:{ [id: number]: any}, id: any){
+    const newObj = {...entities }
+    delete newObj[id]
+    return newObj;
+  }
+}
+interface Entity {
+  id: any;
+}
+
+// const array = [1,2,3,4]
+// const result = array.reduce((previousValue, currentValue) => {
+//   return previousValue + currentValue
+// }, 10)
+// console.log('result = '+ 20);
